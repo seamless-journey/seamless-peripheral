9c22317033beca2b51a147ce1d9970aec0aac0dc
Jeronimo Vallelunga <jeronimo.vallelunga@gmail.com>
2017-11-29 19:12:59 -0300

    Fork: Redux Store

--- src/index.js
+++ src/index.js
@@ -5,12 +5,8 @@ const broadcast = fork('build/broadcast/index.js');
 
 broadcast.on('message', ({ err, shortUrl }) => {
   if (!err) { 
-    const store = fork('build/store/index.js'); 
+    fork('build/store/index.js'); 
   }
 });
 
-setTimeout(() => { 
-  broadcast.send('https://jvallelunga.github.io/redux-bluetooth/');
-}, 10000);
-
-broadcast.send('https://www.npmjs.com/package/google-url');
+broadcast.send('https://jvallelunga.github.io/redux-bluetooth/');
